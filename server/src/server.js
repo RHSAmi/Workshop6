0f0fc9ef07aa853c12e055b4893d0f49f582f94c
Ami Kuehl <tkuehl@umass.edu>
2016-11-30 15:42:35 -0500

    express

---
+++ server/src/server.js
@@ -0,0 +1,28 @@
+//import reverse stuff
+var reverseModule = require('./util');
+var reverseString = reverseModule.reverseString;
+// Imports the express Node module.
+var express = require('express');
+// Creates an Express server.
+var app = express();
+// imports the parser for req.body
+var bodyParser = require('body-parser');
+app.use(bodyParser.text());
+// Defines what happens when it receives the `GET /` request
+app.get('/', function (req, res) {
+  res.send('Hello World!');
+});
+// Handle POST /reverse [data]
+app.post('/reverse', function (req, res) {
+  if (typeof(req.body) === 'string') {
+    var reversed = reverseString(req.body);
+    res.send(reversed);
+  } else {
+    // POST did not contain a string. Send an error code back!
+    res.status(400).end();
+  }
+});
+// Starts the server on port 3000!
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
